c34df169255936325fac7bb6d0979f0eb3d2b921
Phil-NHS <philip.tate@nhs.net>
2025-08-01 11:01:39 +0100

    chore(merge): from master

--- TELBlazor.Components/TELBlazorPackageVersion/VersionInfo.TELBlazor.cs
+++ TELBlazor.Components/TELBlazorPackageVersion/VersionInfo.TELBlazor.cs
@@ -1,2 +1,2 @@
-﻿namespace TELBlazor.Components.TELBlazorPackageVersion{public static partial class VersionInfo{public const string TELBlazorPackageVersion = "1.9.2-local" ;
+﻿namespace TELBlazor.Components.TELBlazorPackageVersion{public static partial class VersionInfo{public const string TELBlazorPackageVersion = "1.9.3-local" ;
 } }
